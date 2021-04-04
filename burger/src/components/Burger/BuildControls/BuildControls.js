6055067e126711f8f0b193cec8ff9d48c6f839b1
tarunraghav226 <tarun.raghav226@gmail.com>
2021-04-04 19:11:18 +0530

    Added order now button.

--- burger/src/components/Burger/BuildControls/BuildControls.js
+++ burger/src/components/Burger/BuildControls/BuildControls.js
@@ -21,6 +21,7 @@ const buildControls = (props) => {
                         removed={() => props.ingredientRemoved(ctrl.type)}
                         disable={props.disableInfo[ctrl.type]}/>
         })}
+        <button className={styles.OrderButton} disabled={!props.purchaseable}>ORDER NOW</button>
     </div>;
 };
 
