2f52094cd349f29096184bf3f50c6889fd6ca807
tarunraghav226 <tarun.raghav226@gmail.com>
2021-04-04 17:54:29 +0530

    Added build control buttons.

--- burger/src/components/Burger/Burger.js
+++ burger/src/components/Burger/Burger.js
@@ -5,7 +5,7 @@ import BurgerIngredients from './BurgerIngredients/BurgerIngredients'
 
 const burger = (props) => {
 
-    const transformedIngredients = 
+    let transformedIngredients = 
                     Object.keys(props.ingredients).map( igKey => {
                         return [...Array(props.ingredients[igKey])].map(
                             (_, index) => {
@@ -14,7 +14,13 @@ const burger = (props) => {
                                             type={igKey}/>
                             }
                         )
-                    })
+                    }).reduce((arr, el) => {
+                        return arr.concat(el);
+                    }, []);
+
+    if(transformedIngredients.length === 0){
+        transformedIngredients = <p>Please add ingredients!!!</p>
+    }
 
     return (
         <div className={styles.Burger}>
