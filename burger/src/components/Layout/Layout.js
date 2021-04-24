031f88887aedbccb3e383dd934fc54ea0f7dbc7b
tarunraghav226 <tarun.raghav226@gmail.com>
2021-04-25 00:15:09 +0530

    Added navigation

--- burger/src/components/Layout/Layout.js
+++ burger/src/components/Layout/Layout.js
@@ -1,8 +1,12 @@
 import React from 'react';
 
+import Toolbar from '../Navigation/Toolbar/Toolbar';
+import styles from './Layout.module.css';
+
 const layout = (props) => {
     return <>
-        <div>
+        <Toolbar/>
+        <div className={styles.Content}>
             Toolbar, Sidebar, Backdrop
         </div>
         <main>
