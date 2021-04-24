43fb8d486ae989f59baba2a040dc17ce289305f9
tarunraghav226 <tarun.raghav226@gmail.com>
2021-04-24 23:36:52 +0530

    Added price prop in order summary.

--- burger/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -95,7 +95,7 @@ class BurgerBuilder extends Component{
                     <OrderSummary ingredients={this.state.ingredients}
                     purchaseCancel={this.purchaseCancleHandler}
                     purchaseContinue={this.purchaseContinueHandler}
-                    price={this.state.totalPrice}/>
+                    price={this.state.totalPrice.toFixed(2)}/>
                 </Modal>
                 <Burger ingredients={this.state.ingredients}/>
                 <BuildControls
